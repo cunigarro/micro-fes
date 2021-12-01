dc4792008013b39e18f3d1df2249ed0c0cbad13d
Christian Unigarro <christian.unigarro@zinobe.com>
2021-11-30 22:32:26 -0500

    Components adapted to Tangelo project.

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, ViewChild, ViewContainerRef, ɵrenderComponent as renderComponent, Inject, Injector, ComponentFactoryResolver } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'shell';
